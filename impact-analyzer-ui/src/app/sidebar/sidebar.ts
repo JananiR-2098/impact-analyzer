7d89645c87d0c6f642c37f01f932e2c215f0ec8c
Janani R <janur2098@gmail.com>
2025-11-19 12:38:09 +0530

    pdf generator for graph

--- impact-analyzer-ui/src/app/sidebar/sidebar.ts
+++ impact-analyzer-ui/src/app/sidebar/sidebar.ts
@@ -1,17 +1,21 @@
-import { Component,OnInit, Output, EventEmitter } from '@angular/core';
+import { Component,OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Sharedservice } from '../services/sharedservice';
 import { GraphResponse } from '../models/graph-response';
 import { Graph } from '../graph/graph';
+
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 interface Msg { role: 'user' | 'assistant'; text: string }
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.html',
-  imports: [CommonModule, MatSidenavModule, MatButtonModule, Graph],
+  imports: [CommonModule, MatSidenavModule, MatButtonModule, MatIconModule, Graph],
   styleUrls: ['./sidebar.css']
 })
 
@@ -21,9 +25,7 @@ export class SidebarComponent  {
   graphData: GraphResponse | null = null;
   panelData: any = null;
 
-  constructor(private sharedservice: Sharedservice) {
-
-  }
+  constructor(private sharedservice: Sharedservice) {}
 
   ngOnInit() {
     this.sharedservice.panelData$.subscribe(data => {
@@ -34,6 +36,20 @@ export class SidebarComponent  {
         console.log("Received panel data:", data);
         console.log("GRAPH:", this.graphData);
       }
+    });
+  }
+
+  @ViewChild('pdfContent', { static: false }) pdfContent!: ElementRef;
+  downloadPDF() {
+    const DATA = this.pdfContent.nativeElement;
+    html2canvas(DATA).then(canvas => {
+      const imgWidth = 208;
+      const pageHeight = 295;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const contentDataURL = canvas.toDataURL('image/png');
+      let pdf = new jsPDF('p', 'mm', 'a4');
+      pdf.addImage(contentDataURL, 'PNG', 0, 0, imgWidth, imgHeight);
+      pdf.save('download.pdf');
     });
   }
 
